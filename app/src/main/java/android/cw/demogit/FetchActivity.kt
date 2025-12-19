1016834d3b538a9a4c6ceda634aec4df3179584a
Pheng Boon <tanphengboon@outlook.sg>
2025-12-19 19:18:12 +0800

    demo create new activity

---
+++ app/src/main/java/android/cw/demogit/FetchActivity.kt
@@ -0,0 +1,26 @@
+package android.cw.demogit
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class FetchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_fetch)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    val fetchBtn = findViewById<Button>(R.id.fetch_button)
+    fetchBtn.setOnClickListener {
+        // etc. etc.
+    }
+}
